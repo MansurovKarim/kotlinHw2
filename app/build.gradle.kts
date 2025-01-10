f9527ad5165c973abde7fd84b0e405bb9b17d29e
Karim <tonygrandson420@gmail.com>
2025-01-11 00:17:47 +0600

    ф

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.kotlinhw2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.kotlinhw2"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
